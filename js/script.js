6acc86b459059de03c8504bc8adefdf879509d58
Megh16ana <gunnatham.meghana@gmail.com>
2023-07-07 20:09:56 +0530

    h

--- js/script.js
+++ js/script.js
@@ -81,3 +81,20 @@ var swiper = new Swiper(".brand-slider", {
         },
     },
 });
+
+$(document).ready(function() {
+    $('#calendar').evoCalendar({
+              'calendarEvents': [
+                      {
+                           id: '4hducye', // Event's id (required, for removing event)
+                           description: 'Lorem ipsum dolor sit amet..', // Description of event (optional)
+                           badge: '1-day event', // Event badge (optional)
+                           date: new Date(), // Date of event
+                           type: 'holiday', // Type of event (event|holiday|birthday)
+                           color: '#63d867', // Event custom color (optional)
+                           everyYear: true // Event is every year (optional)
+                      }
+                 ]
+            
+    })
+})
